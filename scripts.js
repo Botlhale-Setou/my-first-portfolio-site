26c4c0e737f12a3121afebc8562a1a2709433bbf
Daphineatim <hopedaphine19@gmail.com>
2022-09-06 12:26:39 +0300

    Fix linters

--- scripts.js
+++ scripts.js
@@ -1,38 +1,38 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu-nav");
-const portfolio = document.querySelector(".folioport");
-const about = document.querySelector(".bouta");
-const contact = document.querySelector(".tactcon");
-const body = document.querySelector("body");
-const logo = document.querySelector(".logo-text");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu-nav');
+const portfolio = document.querySelector('.folioport');
+const about = document.querySelector('.bouta');
+const contact = document.querySelector('.tactcon');
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo-text');
 
-hamburger.addEventListener("click", (e) => {
-	body.classList.toggle("bang");
-	navMenu.classList.toggle('boom');
-	logo.classList.toggle("invisible");
-})
+hamburger.addEventListener('click', (e) => {
+  body.classList.toggle('bang');
+  navMenu.classList.toggle('boom');
+  logo.classList.toggle('invisible');
+});
 
-portfolio.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
+portfolio.addEventListener('click', (e) => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
 
-about.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
+about.addEventListener('click', (e) => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
 
-contact.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
+contact.addEventListener('click', (e) => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
 
 function myFunction(x) {
-  x.classList.toggle("change");
+  x.classList.toggle('change');
 }
