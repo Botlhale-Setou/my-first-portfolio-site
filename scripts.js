33cfc90a63c4520b10ad5d0e5504fdd11df90a86
Botlhale Setou <botlhale.setou@gmail.com>
2022-09-07 11:39:42 +0200

    Modify files for linter compliance

--- scripts.js
+++ scripts.js
@@ -1,38 +1,39 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu-nav");
-const portfolio = document.querySelector(".folioport");
-const about = document.querySelector(".bouta");
-const contact = document.querySelector(".tactcon");
-const body = document.querySelector("body");
-const logo = document.querySelector(".logo-text");
-
-hamburger.addEventListener("click", (e) => {
-	body.classList.toggle("bang");
-	navMenu.classList.toggle('boom');
-	logo.classList.toggle("invisible");
-})
-
-portfolio.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
-
-about.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
-
-contact.addEventListener("click", (e) => {
-	navMenu.classList.toggle('boom');
-	body.classList.toggle("bang");
-	logo.classList.toggle("invisible");
-	hamburger.classList.toggle("change");
-})
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu-nav');
+const portfolio = document.querySelector('.folioport');
+const about = document.querySelector('.bouta');
+const contact = document.querySelector('.tactcon');
+const body = document.querySelector('body');
+const logo = document.querySelector('.logo-text');
 
 function myFunction(x) {
-  x.classList.toggle("change");
+  x.classList.toggle('change');
 }
+
+hamburger.addEventListener('click', () => {
+  body.classList.toggle('bang');
+  navMenu.classList.toggle('boom');
+  logo.classList.toggle('invisible');
+  myFunction(hamburger);
+});
+
+portfolio.addEventListener('click', () => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
+
+about.addEventListener('click', () => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
+
+contact.addEventListener('click', () => {
+  navMenu.classList.toggle('boom');
+  body.classList.toggle('bang');
+  logo.classList.toggle('invisible');
+  hamburger.classList.toggle('change');
+});
